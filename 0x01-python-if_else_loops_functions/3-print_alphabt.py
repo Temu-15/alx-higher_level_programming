88c9344acdac9caa18d6655b96fde492e12352a1
Temu-15 <abuadanu89@gmail.com>
2023-03-30 11:03:00 +0000

    bugs fixed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(97,123):
+for i in range(97, 123):
     if (i == 113) or (i == 101):
         continue
-    else:
-         print("{:c}".format(i), end='')
+    else:																												print("{}".format(chr(i)), end='')
