6942ab3261c75b2550001ec2996a92724adfce31
Temu-15 <abuadanu89@gmail.com>
2023-03-31 07:28:53 +0000

    added a new line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
     if i == 99:
-        print("{:d}".format(i), end=' ')
+        print("{:d}".format(i), end=' ' end='\n')
     else:
         print("{:02d},".format(i), end=' ')
