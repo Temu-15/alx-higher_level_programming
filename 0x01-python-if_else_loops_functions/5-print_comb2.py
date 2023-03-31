42a16764d7786b46cdb8bfa3441303e54d94f8f3
Temu-15 <abuadanu89@gmail.com>
2023-03-31 07:10:10 +0000

    removed the unnecessary comma

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(0,100):
-    if i < 10 :
-        print("0{:d},".format(i) , end = ' ')
+for i in range(0, 100):
+    if i < 10:
+        print("0{:d},".format(i), end=' ')
+    elif i == 99:
+        print("{:d}".format(i), end=' ')
     else:
-        print("{:d},".format(i) , end = ' ')
+        print("{:d},".format(i), end=' ')
