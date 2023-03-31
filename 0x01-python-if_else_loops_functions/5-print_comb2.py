436d95b61189b27099a005a7821d6a319ba955b2
Temu-15 <abuadanu89@gmail.com>
2023-03-31 07:15:51 +0000

    two print statments

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print("0{:d},".format(i), end=' ')
-    elif i == 99:
+for i in range(00, 100):
+    if i == 99:
         print("{:d}".format(i), end=' ')
     else:
-        print("{:d},".format(i), end=' ')
+        print("{:02d},".format(i), end=' ')
